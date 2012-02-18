d37620ed86b23570fce989a8dfd28458868614a7
Mickael Daniel <daniel.mickael@gmail.com>
2012-02-18 12:59:32 +0100

    templates should be registered only once and without extension

--- test/partials-spec.js
+++ test/partials-spec.js
@@ -21,17 +21,16 @@ vows.describe("Partials").addBatch({
 
     "should register appropriately": function (err, c, src) {
       assert.ifError(err);
-      console.log(Object.keys(c.require.modules));
 
       assert.ok(c.require.modules.handlebars, 'should handlebars runtime module be registered');
       assert.ok(c.require('handlebars'), 'should handlebars runtime module be registered');
 
-      var tmpl = c.require('test/fixtures/foobar.html');
+      var tmpl = c.require('test/fixtures/foobar');
       assert.equal(typeof tmpl, 'function');
       assert.equal(tmpl({}), fs.readFileSync('test/fixtures/expected/foobar-empty.html', 'utf8'));
       assert.equal(tmpl({ title: 'templatify', body: '☺' }), fs.readFileSync('test/fixtures/expected/foobar.html', 'utf8'));
 
-      var partials = c.require('test/fixtures/withpartial.html');
+      var partials = c.require('test/fixtures/withpartial');
       assert.equal(typeof tmpl, 'function');
       assert.equal(partials({}), fs.readFileSync('test/fixtures/expected/partial-empty.html', 'utf8'));
       assert.equal(partials({ adjective: 'templatify' }), fs.readFileSync('test/fixtures/expected/partial.html', 'utf8'));
