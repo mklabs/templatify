e72985271db7fe83156af2d9d550d4a679d67f43
Mickael Daniel <daniel.mickael@gmail.com>
2012-02-25 17:11:53 +0100

    tests - add tests for hogan adapter

--- test/hogan-test.js
+++ test/hogan-test.js
@@ -27,12 +27,59 @@ vows.describe("Hogan adapter").addBatch({
       var tmpl = c.require('test/fixtures/hogan/template'),
         expected = path.join(__dirname, 'fixtures/hogan/expected/template.html');
 
-      assert.equal(tmpl({
+      var got = tmpl({
         name: "Chris",
         value: 10000,
         taxed_value: 10000 - (10000 * 0.4),
-        in_ca: true
-      }), fs.readFileSync(expected, 'utf8'));
+        in_ca: true,
+        items: [{
+          name: 'foobar',
+          parts: [{
+            name: 'foo',
+            val: 'bar'
+          }]
+        }]
+      });
+
+      assert.equal(got, fs.readFileSync(expected, 'utf8'));
+    },
+
+
+    "should have vars and helpers attached as function property": function (err, c, src) {
+      var tmpl = c.require('test/fixtures/hogan/template');
+
+      var got = tmpl({
+        name: "Chris",
+        value: 10000,
+        taxed_value: 10000 - (10000 * 0.4),
+        in_ca: true,
+        items: [{
+          name: 'foobar',
+          parts: [{
+            name: 'foo',
+            val: 'bar'
+          }]
+        }]
+      });
+      assert.ok(tmpl.vars, 'should have vars attached');
+      assert.ok(tmpl.partials, 'should have partials attached');
+
+      var expected = [
+        'name',
+        'value',
+        'in_ca.taxed_value',
+        'in_ca',
+        'items.name',
+        'items.value',
+        'items.parts.name',
+        'items.parts.value',
+        'items.parts',
+        'items',
+        'hash.key',
+      ];
+
+      assert.deepEqual(tmpl.vars, expected, 'should vars be accurate');
+      assert.deepEqual(tmpl.partials, ['whatev', 'another', 'app_template_partial', 'app_template_item'], 'should partials array be accurate');
     }
   }
 }).export(module);
